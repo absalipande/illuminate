e52262a850feb7607f9447510fb29db6e6e0b3ee
Amiel Brencis Salipande <absalipande@gmail.com>
2023-07-19 22:54:34 +0800

    feat: Initial commit for sidebar

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import { ClerkProvider } from '@clerk/nextjs';
+import Head from 'next/head';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -17,6 +18,9 @@ export default function RootLayout({
 }) {
   return (
     <ClerkProvider>
+      <Head>
+        <link rel='icon' href='/illuminate.png' />
+      </Head>
       <html lang='en'>
         <body className={inter.className}>{children}</body>
       </html>
