1bd21094cc569aef7622990f464e0d84a1a23484
Amiel Brencis Salipande <absalipande@gmail.com>
2023-07-20 17:35:46 +0800

    feat: Implemented Dashboard functionality

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,9 +1,42 @@
-const DashboardPage = () => {
+'use client';
+
+import { useRouter } from 'next/navigation';
+import { tools } from '@/constant';
+import { Card } from '@/components/ui/card';
+import { cn } from '@/lib/utils';
+import { ArrowRight } from 'lucide-react';
+
+export default function HomePage() {
+  const router = useRouter();
+
   return (
     <div>
-      <p>Dashboard Page (Protected)</p>
+      <div className='mb-8 space-y-4'>
+        <h2 className='text-2xl md:text-4xl font-bold text-center'>
+          Step into the Future with AI
+        </h2>
+        <p className='text-muted-foreground font-light text-sm md:text-lg text-center'>
+          Unleash the full potential of Artifical Intelligence. <br />
+          Engage, create, compose, and code - all in one powerful platform
+        </p>
+      </div>
+      <div className='px-4 md:px-20 lg:px-32 space-y-4'>
+        {tools.map((tool) => (
+          <Card
+            onClick={() => router.push(tool.href)}
+            key={tool.href}
+            className='p-4 border-black/5 flex items-center justify-between hover:shadow-md transition cursor-pointer'
+          >
+            <div className='flex items-center gap-x-4'>
+              <div className={cn('p-2 w-fit rounded-md', tool.bgColor)}>
+                <tool.icon className={cn('w-8 h-8', tool.color)} />
+              </div>
+              <div className='font-semibold'>{tool.label}</div>
+            </div>
+            <ArrowRight className='w-5 h-5' />
+          </Card>
+        ))}
+      </div>
     </div>
   );
-};
-
-export default DashboardPage;
+}
